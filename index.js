3a3416af1da7e3fe06c9e848fa8b73eedf42effd
Guhan-M <guhan76guhan@gmail.com>
2024-01-22 14:25:23 +0530

    Node 1st

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import fs from "fs"
+const app=express();
+const PORT=process.env.PORT|| 8000
+app.use(express.json())
+
+app.post('/createFile',(req,res)=>{
+    const folderpath ='./textFiles';
+
+    if(!fs.existsSync(folderpath)){
+        (fs.mkdirSync(folderpath))
+    }
+    const timestamp=new Date().toISOString().replace(/:/g,'-');
+    const filename=`${timestamp}.txt`;
+    const filepath=`${folderpath}/${filename}`;
+
+    fs.writeFileSync(filepath,timestamp);
+    res.send("File created");
+});
+
+app.listen(PORT,()=>console.log("App is running"))
+app.get('/getAllFiles',(req,res)=>{
+const files=fs.readdirSync('./textFiles');
+res.json({files})
+});
